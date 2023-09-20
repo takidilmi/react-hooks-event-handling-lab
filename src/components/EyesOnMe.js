7abf60ec577997e6a81109ce8841c5bc28ded4a2
takidilmi <angeldilmi@gmail.com>
2023-09-20 19:30:20 +0200

    Compelted assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+  const handleFocus = () => {
+    console.log("Good!");
+  };
+  const handleBlur = () => {
+    console.log('Hey! Eyes on me!');
+  };
+  
+  return (
+    <div>
+      <button onFocus={handleFocus} onBlur={handleBlur}>
+        <p>Eyes on me</p>
+      </button>
+    </div>
+  );
+}
+
+export default EyesOnMe;
